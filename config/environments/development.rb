070edf1934c7be35732fb5d8e2631728500340bf
Patrick Aviel Peralta <mrpatrickperalta@gmail.com>
2021-06-16 13:19:56 +0800

    fixing rake problem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,6 +30,13 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+
+  begin
+    require 'minitest/autorun'
+    rescue LoadError => e
+    raise e unless ENV['RAILS_ENV'] == "production"
+end
+
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
